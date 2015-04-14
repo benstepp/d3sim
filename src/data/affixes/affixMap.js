85f0067efab4dd80fa22727ef4b7d171229a6158
benstepp <me@benstepp.com>
2015-04-13 19:44:56 -0500

    added legendary belts

--- src/data/affixes/affixMap.js
+++ src/data/affixes/affixMap.js
@@ -184,9 +184,12 @@ var affixMap = {
 		text_en:'Ignores Durability Loss'
 	},
 	'LifeAfterKill':{
+		suffix_en:'of Extermination'
 
 	},
-	'LevelReduc':{},
+	'LevelReduc':{
+		suffix_en:'of Courage'
+	},
 
 
 	//onhit
@@ -195,50 +198,80 @@ var affixMap = {
 		text_en:'{$%} Chance to Blind on Hit',
 		prefix_en:'Perplexing'
 	},
-	'FearHit':{},
-	'ChillHit':{},
-	'SlowHit':{},
-	'KnockHit':{},
-	'StunHit':{},
+	'FearHit':{
+		suffix_en:'of Horror'
+	},
+	'ChillHit':{
+		prefix_en:'Glacial'
+	},
+	'SlowHit':{
+		prefix_en:'Persecuting'
+	},
+	'KnockHit':{
+		prefix_en:'Smashing'
+	},
+	'StunHit':{
+		suffix_en:'of Ruin'
+	},
 
 	//Class based
 	'SpiritRegen':{
 		text_en:'Increases Spirit Regeneration by {} per Second (Monk Only)',
 		prefix_en:'Avaricious'
 	},
+
 	'MaxFury':{
 		text_en:'{+$} Maximum Fury'
 	},
 	'LifePerFury':{
 
 	},
+
+	'LifePerWrath':{
+		prefix_en:'Dedicated'
+	},
+
+	'MaxDisc':{
+		prefix_en:'Confident',
+		exclude:['Monk','Barbarian','Witch Doctor','Wizard','Crusader'],
+	},
 	'HatredRegen':{
+		prefix_en:'Vengeful',
 		exclude:['Chest Armor','Monk','Barbarian','Witch Doctor','Wizard','Crusader']
+	},
+
+	'MaxAp':{
+		prefix_en:'Mental'
 	},
 
 	//SkillEffects
 	'RainOfVengeanceDmg':{
 		text_en:'Increases {Rain of Vengeance} Damage by {$%}',
+		suffix_en:'of Dark Skies',
 		exclude:['Monk','Crusader','Barbarian','Witch Doctor','Wizard',
 		'FanOfKnivesDmg','CompanionDmg','SpikeTrapDmg','SentryDmg']
 	},
 	'FanOfKnivesDmg':{
 		text_en:'Increases {Fan of Knives} Damage by {$%}',
+		suffix_en:'of Concealed Blades',
 		exclude:['Monk','Crusader','Barbarian','Witch Doctor','Wizard',
 		'RainOfVengeanceDmg','CompanionDmg','SpikeTrapDmg','SentryDmg']
 	},
 	'CompanionDmg':{
 		text_en:'Increases {Companion} Damage by {$%}',
+		suffix_en:'Companion',
 		exclude:['Monk','Crusader','Barbarian','Witch Doctor','Wizard',
 		'RainOfVengeanceDmg','FanOfKnivesDmg','SpikeTrapDmg','SentryDmg']
 	},
 	'SpikeTrapDmg':{
 		text_en:'Increases {Spike Trap} Damage by {$%}',
+		suffix_en:'of Spines',
 		exclude:['Monk','Crusader','Barbarian','Witch Doctor','Wizard',
 		'RainOfVengeanceDmg','FanOfKnivesDmg','CompanionDmg','SentryDmg']
 	},
 	'SentryDmg':{
 		text_en:'Increases {Sentry} Damage by {$%}',
+		suffix_en:'of Sentry',
 		exclude:['Monk','Crusader','Barbarian','Witch Doctor','Wizard',
 		'RainOfVengeanceDmg','FanOfKnivesDmg','CompanionDmg','SpikeTrapDmg']
 	},
@@ -334,6 +367,7 @@ var affixMap = {
 	},
 	'GargantuanDmg':{
 		text_en:'Increases {Gargantuan} Damage by {$%}',
+		suffix_en:'of the Great Hulk',
 		exclude:['Monk','Barbarian','Demon Hunter','Wizard','Crusader',
 		'WallOfZombiesDmg','GraspOfTheDeadDmg','HauntDmg','PiranhasDmg','LocustSwarmDmg','FetishArmyDmg','SummonZombieDogsDmg']
 	},
@@ -349,6 +383,7 @@ var affixMap = {
 	},
 	'LocustSwarmDmg':{
 		text_en:'Increases {Locust Swarm} Damage by {$%}',
+		suffix_en:'of Pestilence',
 		exclude:['Monk','Barbarian','Demon Hunter','Wizard','Crusader',
 		'WallOfZombiesDmg','GraspOfTheDeadDmg','GargantuanDmg','HauntDmg','PiranhasDmg','FetishArmyDmg','SummonZombieDogsDmg']
 	},
@@ -440,6 +475,7 @@ var affixMap = {
 	},
 	'ChakramDmg':{
 		text_en:'Increases {Chakram} Damage by {$%}',
+		suffix_en:'of the Boomerang',
 		exclude:['Barbarian','Crusader','Monk','Witch Doctor','Wizard',
 		'ClusterArrowDmg','ElementalArrowDmg','ImpaleDmg','MultishotDmg','RapidFireDmg','StrafeDmg']
 	},
@@ -450,6 +486,7 @@ var affixMap = {
 	},
 	'ElementalArrowDmg':{
 		text_en:'Increases {Elemental Arrow} Damage by {$%}',
+		suffix_en:'of Blasting',
 		exclude:['Barbarian','Crusader','Monk','Witch Doctor','Wizard',
 		'ChakramDmg','ClusterArrowDmg','ImpaleDmg','MultishotDmg','RapidFireDmg','StrafeDmg']
 	},
@@ -460,16 +497,19 @@ var affixMap = {
 	},
 	'MultishotDmg':{
 		text_en:'Increases {Multishot} Damage by {$%}',
+		suffix_en:'of Volleys',
 		exclude:['Barbarian','Crusader','Monk','Witch Doctor','Wizard',
 		'ChakramDmg','ClusterArrowDmg','ElementalArrowDmg','ImpaleDmg','RapidFireDmg','StrafeDmg']
 	},
 	'RapidFireDmg':{
 		text_en:'Increases {Rapid Fire} Damage by {$%}',
+		suffix_en:'of Salvos',
 		exclude:['Barbarian','Crusader','Monk','Witch Doctor','Wizard',
 		'ChakramDmg','ClusterArrowDmg','ElementalArrowDmg','ImpaleDmg','MultishotDmg','StrafeDmg']
 	},
 	'StrafeDmg':{
 		text_en:'Increases {Strafe} Damage by {$%}',
+		suffix_en:'of Prowess',
 		exclude:['Barbarian','Crusader','Monk','Witch Doctor','Wizard',
 		'ChakramDmg','ClusterArrowDmg','ElementalArrowDmg','ImpaleDmg','MultishotDmg','RapidFireDmg']
 	},
@@ -480,6 +520,7 @@ var affixMap = {
 	},
 	'LashingTailKickDmg':{
 		text_en:'Increases {Lashing Tail Kick} Damage by {$%}',
+		suffix_en:'of the Scorpion',
 		exclude:['Barbarian','Crusader','Demon Hunter','Witch Doctor','Wizard',
 		'ExplodingPalmDmg','TempestRushDmg','WaveOfLightDmg']
 	},
@@ -515,6 +556,7 @@ var affixMap = {
 	},
 	'ZombieChargerDmg':{
 		text_en:'Increases {Zombie Charger} Damage by {$%}',
+		suffix_en:'of Blind Rage',
 		exclude:['Barbarian','Crusader','Demon Hunter','Monk','Wizard',
 		'AcidCloudDmg','FirebatsDmg','SacrificeDmg','SpiritBarrageDmg']
 	},
@@ -550,6 +592,7 @@ var affixMap = {
 	},
 	'WaveOfForceDmg':{
 		text_en:'Increases {Wave of Force} Damage by {$%}',
+		suffix_en:'of Repulsion',
 		exclude:['Barbarian','Crusader','Demon Hunter','Monk','Witch Doctor',
 		'ArcaneOrbDmg','ArcaneTorrentDmg','DisintegrateDmg','EnergyTwisterDmg','MeteorDmg','RayOfFrostDmg']
 	},
@@ -601,6 +644,7 @@ var affixMap = {
 	},
 	'EntanglingShotDmg':{
 		text_en:'Increases {Entangling Shot} Damage by {$%}',
+		suffix_en:'of Binding',
 		exclude:['Barbarian','Crusader','Monk','Witch Doctor','Wizard',
 		'BolasDmg','EvasiveFireDmg','GrenadeDmg','HungeringArrowDmg']
 	},
@@ -611,11 +655,13 @@ var affixMap = {
 	},
 	'GrenadeDmg':{
 		text_en:'Increases {Grenade} Damage by {$%}',
+		suffix_en:'of the Grenadier',
 		exclude:['Barbarian','Crusader','Monk','Witch Doctor','Wizard',
 		'BolasDmg','EntanglingShotDmg','EvasiveFireDmg','HungeringArrowDmg']
 	},
 	'HungeringArrowDmg':{
 		text_en:'Increases {Hungering Arrow} Damage by {$%}',
+		suffix_en:'of the Predator',
 		exclude:['Barbarian','Crusader','Monk','Witch Doctor','Wizard',
 		'BolasDmg','EntanglingShotDmg','EvasiveFireDmg','GrenadeDmg']
 	},
@@ -641,6 +687,7 @@ var affixMap = {
 	},
 	'CorpseSpidersDmg':{
 		text_en:'Increases {Corpse Spiders} Damage by {$%}',
+		suffix_en:'of the Black Widow',
 		exclude:['Barbarian','Crusader','Demon Hunter','Monk','Wizard',
 		'FirebombDmg','PlagueOfToadsDmg','PoisonDartDmg']
 	},
@@ -671,6 +718,7 @@ var affixMap = {
 	},
 	'ShockPulseDmg':{
 		text_en:'Increases {Shock Pulse} Damage by {$%}',
+		suffix_en:'of Ruin',
 		exclude:['Barbarian','Crusader','Demon Hunter','Monk','Witch Doctor',
 		'ElectrocuteDmg','MagicMissleDmg','SpectralBladeDmg']
 	},
@@ -745,6 +793,58 @@ var affixMap = {
 	'Cassius':{
 		type:'legendary',
 		text_en:'Increases the duration of Ignore Pain by {$} seconds.'
+	},
+	'Frenzy':{
+		type:'legendary',
+		text_en:'Frenzy gains the effect of every rune.'
+	},
+	'Punish':{
+		type:'legendary',
+		text_en:'Punish gains the effect of every rune.'
+	},
+	'Bombard':{
+		type:'legendary',
+		text_en:'Every {$} seconds, call down Bombardment on a random nearby enemy.'
+	},
+	'ShermaBlind':{
+		type:'legendary',
+		text_en:'Chance on hit to create a chaos field that Blinds and Slows enemies inside for {$} seconds.'
+	},
+	'Goldwrap':{
+		type:'legendary',
+		text_en:'On gold pickup: Gain armor for 5 seconds equal to the amount picked up.'
+	},
+	'Harrington':{
+		type:'legendary',
+		text_en:'Opening a chest grants {$%} increased damage for 10 seconds.'
+	},
+	'HwojWrap':{
+		type:'legendary',
+		text_en:'Locust Swarm also Slows enemies by {$%}'
+	},
+	'Jangs':{
+		type:'legendary',
+		text_en:'Enemies damaged by Black Hole are also slowed by {$%} for 3 seconds.'
+	},
+	'KrelmBelt':{
+		type:'legendary',
+		text_en:'Gain {25%} run speed. This effect is lost for 5 seconds after taking damage.'
+	},
+	'RazorStrop':{
+		type:'legendary',
+		text_en:'Picking up a Health Globe releases an explosion that deals {$%} weapon damage as Fire to enemies within 20 yards.'
+	},
+	'SashOfKnives':{
+		type:'legendary',
+		text_en:'With every attack, you throw a dagger at a nearby enemy for {$%} weapon damage as Physical.'
+	},
+	'Sebors':{
+		type:'legendary',
+		text_en:'Haunt is cast on all nearby enemies when you open a chest.'
+	},
+	'Thundergod':{
+		type:'legendary',
+		text_en:'Blocking,dodging or being hit causes you to discharge bolts of electricity that deal {$%} weapon damage as Lightning.'
 	}
 
 
--- src/data/legendary/slot/Belt.js
+++ src/data/legendary/slot/Belt.js
@@ -172,10 +172,17 @@ var Belt = [
 		smartLoot: [
 			"Barbarian"
 		],
-		primary:{},
-		secondary:{},
-		image:'',
-		flavor:''
+		primary:{
+			Strength:null,
+			AllResist:null,
+			RANDOM:2
+		},
+		secondary:{
+			Frenzy:null,
+			RANDOM:1
+		},
+		image:'//media.blizzard.com/d3/icons/items/large/p2_unique_barbbelt_006_demonhunter_male.png',
+		flavor:'Few in this world can truly be called champions.'
 	},
 	{
 		name: "Angel Hair Braid",
@@ -186,10 +193,16 @@ var Belt = [
 		smartLoot: [
 			"Crusader"
 		],
-		primary:{},
-		secondary:{},
-		image:'',
-		flavor:''
+		primary:{
+			Strength:null,
+			RANDOM:3
+		},
+		secondary:{
+			Punish:null,
+			IgnoreDura:null
+		},
+		image:'//media.blizzard.com/d3/icons/items/large/unique_belt_003_p1_demonhunter_male.png',
+		flavor:'“Death is preferable to the fate suffered by those who are taken by the Burning Hells, for theirs is an existence of misery and pain. If they could only strike back against those who imprisoned them, they would die contented.” -The Journals of Deckard Cain'
 	},
 	{
 		name: "Belt of the Trove",
@@ -200,10 +213,23 @@ var Belt = [
 		smartLoot: [
 			"Crusader"
 		],
-		primary:{},
-		secondary:{},
-		image:'',
-		flavor:''
+		primary:{
+			Strength:null,
+			AllResist:null,
+			RANDOM:2
+		},
+		secondary:{
+			MeleeReduc:{
+				min:4,
+				max:4
+			},
+			Bombard:{
+				min:6,
+				max:8
+			}
+		},
+		image:'//media.blizzard.com/d3/icons/items/large/p2_unique_belt_008_demonhunter_male.png',
+		flavor:'Rumors have persisted for centuries that the crusaders maintain a secret bunker in Kehjistan, filled with ancient relics of unbelievable power. When the need is great, Crusaders are allowed to borrow one of these relics. The existence of this crusader belt would seem to confirm the existence of this bunker.'
 	},
 	{
 		name: "Blackthorne's Notched Belt",
@@ -211,6 +237,7 @@ var Belt = [
 		weight: 100,
 		hc: false,
 		season: false,
+		set:'Blackthorne\'s Battlegear',
 		smartLoot: [
 			"Demon Hunter",
 			"Monk",
@@ -219,10 +246,17 @@ var Belt = [
 			"Wizard",
 			"Witch Doctor"
 		],
-		primary:{},
-		secondary:{},
-		image:'',
-		flavor:''
+		primary:{
+			MAIN:null,
+			Vitality:null,
+			RANDOM:2
+		},
+		secondary:{
+			GoldFind:null,
+			RANDOM:1
+		},
+		image:'//media.blizzard.com/d3/icons/items/large/unique_belt_015_x1_demonhunter_male.png',
+		flavor:'A thick leather belt, notched once for each enemy noble captured in battle, each man ransomed back to his family for a handsome fee.'
 	},
 	{
 		name: "Cord of the Sherma",
@@ -238,10 +272,20 @@ var Belt = [
 			"Wizard",
 			"Witch Doctor"
 		],
-		primary:{},
-		secondary:{},
-		image:'',
-		flavor:''
+		primary:{
+			MAIN:null,
+			Vitality:null,
+			RANDOM:2
+		},
+		secondary:{
+			PickupRadius:null,
+			ShermaBlind:{
+				min:3,
+				max:4
+			}
+		},
+		image:'//media.blizzard.com/d3/icons/items/large/unique_belt_104_p2_demonhunter_male.png',
+		flavor:'"During battle, confusion quickly gives way to panic, and a frightened enemy is the easiest to defeat." —Rakkis\'s Strategies of War'
 	},
 	{
 		name: "Goldwrap",
@@ -257,10 +301,16 @@ var Belt = [
 			"Wizard",
 			"Witch Doctor"
 		],
-		primary:{},
-		secondary:{},
-		image:'',
-		flavor:''
+		primary:{
+			MAIN:null,
+			RANDOM:3
+		},
+		secondary:{
+			GoldFind:null,
+			Goldwrap:null
+		},
+		image:'//media.blizzard.com/d3/icons/items/large/unique_belt_010_x1_demonhunter_male.png',
+		flavor:'Crafted in the Amaral Mountains by the Kaniuga artisans, its fibers seem to draw gold from the very air.'
 	},
 	{
 		name: "Harrington Waistguard",
@@ -276,10 +326,19 @@ var Belt = [
 			"Wizard",
 			"Witch Doctor"
 		],
-		primary:{},
-		secondary:{},
-		image:'',
-		flavor:''
+		primary:{
+			MAIN:null,
+			RANDOM:3
+		},
+		secondary:{
+			GoldFind:null,
+			Harrington:{
+				min:100,
+				max:135
+			}
+		},
+		image:'//media.blizzard.com/d3/icons/items/large/unique_belt_105_x1_demonhunter_male.png',
+		flavor:'"The thrill of successfully looting a chest makes one feel as if one may conquer any challenge. The Harrington Waistguard turns this feeling of invincibility into a reality." —Lyndon\'s Tales of Fortune and Glory'
 	},
 	{
 		name: "Hellcat Waistguard",
@@ -295,10 +354,20 @@ var Belt = [
 			"Wizard",
 			"Witch Doctor"
 		],
-		primary:{},
-		secondary:{},
-		image:'',
-		flavor:''
+		primary:{
+			MAIN:null,
+			Vitality:null,
+			AttackSpeed:{
+				min:5,
+				max:6
+			},
+			RANDOM:1
+		},
+		secondary:{
+			RANDOM:2
+		},
+		image:'//media.blizzard.com/d3/icons/items/large/unique_belt_005_x1_demonhunter_male.png',
+		flavor:'Forged in the pits of the Burning Hells using dismembered demons as raw materials.'
 	},
 	{
 		name: "Hwoj Wrap",
@@ -309,10 +378,20 @@ var Belt = [
 		smartLoot: [
 			"Witch Doctor"
 		],
-		primary:{},
-		secondary:{},
-		image:'',
-		flavor:''
+		primary:{
+			Intelligence:null,
+			AllResist:null,
+			RANDOM:2
+		},
+		secondary:{
+			HwojWrap:{
+				min:60,
+				max:80
+			},
+			RANDOM:1
+		},
+		image:'//media.blizzard.com/d3/icons/items/large/unique_belt_107_x1_demonhunter_male.png',
+		flavor:'"The witch doctor was eager to display his martial prowess, and he was especially proud of his enchanted belt, which he claimed gave him great advantage in battle. It was obviously an item of great importance to him and had been in his clan for generations untold." —Abd al-Hazir'
 	},
 	{
 		name: "Inna's Favor",
@@ -320,13 +399,20 @@ var Belt = [
 		weight: 100,
 		hc: false,
 		season: false,
+		set:'Inna\'s Mantra',
 		smartLoot: [
 			"Monk"
 		],
-		primary:{},
-		secondary:{},
-		image:'',
-		flavor:''
+		primary:{
+			Dexterity:null,
+			AllResist:null,
+			RANDOM:2
+		},
+		secondary:{
+			RANDOM:2
+		},
+		image:'//media.blizzard.com/d3/icons/items/large/unique_belt_007_x1_demonhunter_male.png',
+		flavor:'Gird yourself with the wisdom of Inna that you may gain her favor.'
 	},
 	{
 		name: "Jang's Envelopment",
@@ -337,10 +423,19 @@ var Belt = [
 		smartLoot: [
 			"Wizard"
 		],
-		primary:{},
-		secondary:{},
-		image:'',
-		flavor:''
+		primary:{
+			Intelligence:null,
+			RANDOM:3
+		},
+		secondary:{
+			Jangs:{
+				min:60,
+				max:80
+			},
+			RANDOM:1
+		},
+		image:'//media.blizzard.com/d3/icons/items/large/unique_belt_106_x1_demonhunter_male.png',
+		flavor:'"In desperation, Irtai cast Black Hole. She was surprised when the flesh hunter\'s movements slowed, allowing her to deliver the killing blow, as she had forgotten she was wearing Jang\'s Envelopment. Irtai was lucky, but one cannot depend on luck. One must always be aware of the abilities of one\'s armor, and those of all the other magical items one carries." —Arch Mage Valthek'
 	},
 	{
 		name: "Krelm's Buff Belt",
@@ -348,6 +443,7 @@ var Belt = [
 		weight: 100,
 		hc: false,
 		season: true,
+		set:'Krelm\'s Buff Bulwark',
 		smartLoot: [
 			"Demon Hunter",
 			"Monk",
@@ -356,10 +452,16 @@ var Belt = [
 			"Wizard",
 			"Witch Doctor"
 		],
-		primary:{},
-		secondary:{},
-		image:'',
-		flavor:''
+		primary:{
+			MAIN:null,
+			RANDOM:3
+		},
+		secondary:{
+			KrelmBelt:null,
+			RANDOM:1
+		},
+		image:'//media.blizzard.com/d3/icons/items/large/unique_belt_set_02_x1_demonhunter_male.png',
+		flavor:'“Whereupon having vanquished the Oglak beast that had devoured a ten-stone weight worth of villagers, didst they hitherto beseech Krelm the Immodest to remain henceforth as the village champion and erected a statue to honor his name hereinafter.” -Excerpt from Stories Meant to Frighten Small Children'
 	},
 	{
 		name: "Razor Strop",
@@ -375,10 +477,19 @@ var Belt = [
 			"Wizard",
 			"Witch Doctor"
 		],
-		primary:{},
-		secondary:{},
-		image:'',
-		flavor:''
+		primary:{
+			MAIN:null,
+			RANDOM:3
+		},
+		secondary:{
+			PickupRadius:null,
+			RazorStrop:{
+				min:300,
+				max:400
+			}
+		},
+		image:'//media.blizzard.com/d3/icons/items/large/unique_belt_101_x1_demonhunter_male.png',
+		flavor:'Encompassing both form and function, this belt keeps your pants up and your blades sharp.'
 	},
 	{
 		name: "Saffron Wrap",
@@ -394,10 +505,23 @@ var Belt = [
 			"Wizard",
 			"Witch Doctor"
 		],
-		primary:{},
-		secondary:{},
-		image:'',
-		flavor:''
+		primary:{
+			MAIN:null,
+			CostReduc:{
+				min:4,
+				max:8
+			},
+			RANDOM:2
+		},
+		secondary:{
+			CcReduc:{
+				min:4,
+				max:8
+			},
+			RANDOM:1
+		},
+		image:'//media.blizzard.com/d3/icons/items/large/unique_belt_001_x1_demonhunter_male.png',
+		flavor:'Bound according to the precepts of Anrach.'
 	},
 	{
 		name: "Sash of Knives",
@@ -413,10 +537,19 @@ var Belt = [
 			"Wizard",
 			"Witch Doctor"
 		],
-		primary:{},
-		secondary:{},
-		image:'',
-		flavor:''
+		primary:{
+			MAIN:null,
+			RANDOM:3
+		},
+		secondary:{
+			SashOfKnives:{
+				min:500,
+				max:650
+			},
+			RANDOM:1
+		},
+		image:'//media.blizzard.com/d3/icons/items/large/unique_belt_102_p2_demonhunter_male.png',
+		flavor:'“Keep your friends close, and your enemies within dagger throwing range.” Vis-Jaq\'taar saying.'
 	},
 	{
 		name: "Sebor's Nightmare",
@@ -432,10 +565,16 @@ var Belt = [
 			"Wizard",
 			"Witch Doctor"
 		],
-		primary:{},
-		secondary:{},
-		image:'',
-		flavor:''
+		primary:{
+			MAIN:null,
+			RANDOM:3
+		},
+		secondary:{
+			PickupRadius:null,
+			Sebors:null
+		},
+		image:'//media.blizzard.com/d3/icons/items/large/unique_belt_108_p2_demonhunter_male.png',
+		flavor:'Sebor\'s Nightmare can easily make the difference between staying in this land or passing on to the next. For, though highly unlikely, the opening of a chest can have dire results on occasion.'
 	},
 	{
 		name: "String of Ears",
@@ -451,10 +590,19 @@ var Belt = [
 			"Wizard",
 			"Witch Doctor"
 		],
-		primary:{},
-		secondary:{},
-		image:'',
-		flavor:''
+		primary:{
+			MAIN:null,
+			RANDOM:3
+		},
+		secondary:{
+			MeleeReduc:{
+				min:25,
+				max:30
+			},
+			RANDOM:1
+		},
+		image:'//media.blizzard.com/d3/icons/items/large/unique_belt_008_x1_demonhunter_male.png',
+		flavor:'A proud fighter displays the ears of those he has slain in battle.'
 	},
 	{
 		name: "Tal Rasha's Brace",
@@ -462,13 +610,20 @@ var Belt = [
 		weight: 100,
 		hc: false,
 		season: false,
+		set:'Tal Rasha\'s Elements',
 		smartLoot: [
 			"Wizard"
 		],
-		primary:{},
-		secondary:{},
-		image:'',
-		flavor:''
+		primary:{
+			Intelligence:null,
+			AllResist:null,
+			RANDOM:2
+		},
+		secondary:{
+			RANDOM:2
+		},
+		image:'//media.blizzard.com/d3/icons/items/large/unique_belt_006_x1_demonhunter_male.png',
+		flavor:'Every aspect of the Horadrim\'s dress was carefully chosen to augment his magical abilities.'
 	},
 	{
 		name: "The Witching Hour",
@@ -484,10 +639,23 @@ var Belt = [
 			"Wizard",
 			"Witch Doctor"
 		],
-		primary:{},
-		secondary:{},
-		image:'',
-		flavor:''
+		primary:{
+			MAIN:null,
+			AttackSpeed:{
+				min:5,
+				max:6
+			},
+			CritDamage:{
+				min:26,
+				max:50
+			},
+			RANDOM:1
+		},
+		secondary:{
+			RANDOM:2
+		},
+		image:'//media.blizzard.com/d3/icons/items/large/unique_belt_009_x1_demonhunter_male.png',
+		flavor:'When the moon is full and silence descends upon the land, the hour of magic is upon us.'
 	},
 	{
 		name: "Thundergod's Vigor",
@@ -503,10 +671,24 @@ var Belt = [
 			"Wizard",
 			"Witch Doctor"
 		],
-		primary:{},
-		secondary:{},
-		image:'',
-		flavor:''
+		primary:{
+			MAIN:null,
+			LightningDamage:{
+				min:10,
+				max:15
+			},
+			Vitality:null,
+			RANDOM:1
+		},
+		secondary:{
+			LightRes:null,
+			Thundergod:{
+				min:100,
+				max:130
+			}
+		},
+		image:'//media.blizzard.com/d3/icons/items/large/unique_barbbelt_003_x1_demonhunter_male.png',
+		flavor:'"And the god of thunder struck down transgressors with great fury and vengeance..." —Scrolls of Haroutunian'
 	},
 	{
 		name: "Vigilante Belt",
@@ -522,10 +704,19 @@ var Belt = [
 			"Wizard",
 			"Witch Doctor"
 		],
-		primary:{},
-		secondary:{},
-		image:'',
-		flavor:''
+		primary:{
+			MAIN:null,
+			CdReduc:{
+				min:5,
+				max:8
+			},
+			RANDOM:2
+		},
+		secondary:{
+			RANDOM:2
+		},
+		image:'//media.blizzard.com/d3/icons/items/large/unique_belt_002_x1_demonhunter_male.png',
+		flavor:'He stalked his prey by dark of night, separating the evil ones from their lives.'
 	}
 ];
 
