838316d9020048e8f2cd986914e7bf239488e1dc
benstepp <me@benstepp.com>
2015-04-30 19:42:03 -0500

    well we run out of memory testing 10000 items for possibility. hmmmmm

--- test/item.js
+++ test/item.js
@@ -52,5 +52,23 @@ function itemTest(item) {
 	});
 }
 
-d3sim.setKadala('Monk',true,true);
-itemTest(d3sim.kadalaRoll('twohand'));
+function test() {
+	var dClasses = ['Barbarian','Crusader','Monk','Demon Hunter','Wizard','Witch Doctor'];
+	var dClassLength = dClasses.length;
+	var slots = ['amulet','belt','boots','bracers','chest','gloves','helm','pants','ring','shoulders','mojo','shield','onehand','quiver','source','twohand'];
+	var slotLength = slots.length;
+
+	for (var y = 0; y < dClassLength; y++) {
+		console.log(dClasses[y]);
+		d3sim.setKadala(dClasses[y], true, true);
+		for (var t = 0; t < slotLength; t++) {
+			console.log(slots[t]);
+			for (var i =0; i < 100; i++) {
+				var it = d3sim.kadalaRoll(slots[t]);
+				itemTest(it);
+			}
+		}
+	}
+}
+
+test();
